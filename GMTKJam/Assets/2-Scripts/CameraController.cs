f8e4ec81eb039409f17a6d074226d011e8202a49
BlindEagle <>
2020-07-01 19:16:03 +0300

    added camera script

---
+++ GMTKJam/Assets/2-Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 positionOffset;
+    [Range(0f, 1f)] public float smoothTime;
+    public float sensitivity;
+
+    private void LateUpdate() {
+        transform.position = Vector3.Lerp(transform.position, target.position + positionOffset, smoothTime);
+        transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
+    }
+}
